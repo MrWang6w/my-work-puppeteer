51e957512cd8ef22e2caeddcd956ad5507b3873b
王亮 <wangliang15@shein.com>
2023-04-07 15:25:02 +0800

    feat: 尝试使用playwright

--- index.js
+++ index.js
@@ -1,46 +1,26 @@
 const express = require('express');
-const puppeteer = require('puppeteer');
+const playwright = require('playwright');
 
 const app = express();
-
-
-let browser;
-
-async function getBrowserInstance() {
-  if (!browser) {
-    browser = await puppeteer.launch({
-      args: ['--no-sandbox']
-    });
-  }
-  return browser;
-}
-
+const port = process.env.PORT || 3000;
 
 app.get('/', async (req, res) => {
-  const { url } = req.query;
+  const url = req.query.url;
+  if (!url) {
+    return res.status(400).send('Missing url parameter');
+  }
 
-  // 创建浏览器实例
-  const oneBrowser = await getBrowserInstance()
+  const browser = await playwright.chromium.launch();
+  const context = await browser.newContext();
+  const page = await context.newPage();
+  await page.goto(url);
+  const screenshotBuffer = await page.screenshot();
+  await browser.close();
 
-  // 打开一个新页面
-  const page = await oneBrowser.newPage();
-
-  // 进行页面访问
-  await page.goto(url, { waitUntil: 'networkidle2' });
-
-  // 截取图片
-  const screenshot = await page.screenshot({ type: 'png' });
-
-  // 关闭浏览器实例
-  await oneBrowser.close();
-
-  // 设置返回的内容为图片
-  res.setHeader('Content-Type', 'image/png');
-  res.send(screenshot);
+  res.set('Content-Type', 'image/png');
+  res.send(screenshotBuffer);
 });
 
-// 启动服务
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+app.listen(port, () => {
+  console.log(`Server listening at http://localhost:${port}`);
 });
